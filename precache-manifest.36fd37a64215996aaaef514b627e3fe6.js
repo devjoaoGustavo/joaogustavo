24c085500ac7655df171181d818f366b24b81d3a
João Gustavo de Paula <dev.joaogustavo@gmail.com>
2019-01-15 22:03:32 -0200

    initial gh-pages

---
+++ precache-manifest.36fd37a64215996aaaef514b627e3fe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ec7502e494bf496574b",
+    "url": "/joaogustavo/js/about.5273d30c.js"
+  },
+  {
+    "revision": "f62466af2ed5aed85bf0",
+    "url": "/joaogustavo/css/app.4383fdc6.css"
+  },
+  {
+    "revision": "f62466af2ed5aed85bf0",
+    "url": "/joaogustavo/js/app.6c04ba72.js"
+  },
+  {
+    "revision": "d3820bbbd7739c95746f",
+    "url": "/joaogustavo/js/chunk-vendors.05561806.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/joaogustavo/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8eb025e7ca545348ac35ddd3487b77ee",
+    "url": "/joaogustavo/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/joaogustavo/robots.txt"
+  }
+];
