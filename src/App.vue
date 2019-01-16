b6be60a3bb31ee566df2d05e9eda5ccaf2d63bf6
João Gustavo de Paula <joao.paula@locaweb.com.br>
2019-01-16 13:11:05 -0200

    logo

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <v-img :src="require('./assets/million-logo.svg')" width="64" height="64"></v-img>
+        <v-img :src="require('./assets/web-logo.svg')" width="64" height="64"></v-img>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn depressed small to="/">Home</v-btn> | <v-btn depressed small to="/about">About</v-btn>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -83,6 +83,7 @@
         </v-layout>
       </v-flex>
     </v-layout>
+    <div>Icons made by <a href="https://www.flaticon.com/authors/icongeek26" title="Icongeek26">Icongeek26</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
   </v-container>
 </template>
 
